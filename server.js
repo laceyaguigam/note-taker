c2dfe66eee91b49ca6c754d21f7242c6d97a5a80
lacey aguigam <laceyaguigam@yahoo.com>
2022-10-26 18:28:53 -0700

    challenge 11 code

--- server.js
+++ server.js
@@ -5,30 +5,34 @@ const path =require('path')
 
 const PORT = process.env.PORT || 3001;
 
-const { notes } = require(``);
+const notes = require(`./db/db.json`);
 
 //WHEN I open the Note Taker
 //THEN I am presented with a landing page with a link to a notes page
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, ))
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
 
 
-
-
-app.get(``, (req, res) => {
-  //  let resuts = ;
-    return results;
-
-    res.json(results);
-});
+ app.get(`/api/notes`, (req, res) => {
+   res.json(notes);
+ });
 
 //WHEN I click on the link to the notes page
 //THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title 
 
-app.post(``, (req, res) => {
-
+app.post(`/api/notes`, (req, res) => {
+notes.push(req.body)
+res.json(notes)
 });
 
 //WHEN I enter a new note title and the note’s text
